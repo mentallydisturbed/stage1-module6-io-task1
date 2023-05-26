dbbdf4bf93a1bc353b2ec2ff0cd0c2853b74694a
mentallydisturbed <magzhan.zhumagazy@gmail.com>
2023-05-27 02:16:51 +0600

    kek4

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -3,7 +3,6 @@ package com.epam.mjc.io;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.Logger;
 
 
 public class FileReader {
@@ -17,18 +16,23 @@ public class FileReader {
             boolean has = false;
             while((c = in.read()) != -1){
                 if((char)(c) == '\n') {
+                    int len = val.length();
                     if(tp.toString().contains("Name:")){
-                        profile.setName(val.substring(0, val.length() - 1));
+                        if(!Character.isDigit(val.charAt(len - 1)) && !Character.isAlphabetic(val.charAt(len - 1)))
+                            len--;
+                        profile.setName(val.substring(0, len));
                     }else if(tp.toString().contains("Age:")) {
-                        if(Character.isDigit(val.charAt(val.length() - 1)))
-                            profile.setAge(Integer.parseInt(val.toString()));
-                        else profile.setAge(Integer.parseInt(val.substring(0, val.length() - 1)));
+                        if(!Character.isDigit(val.charAt(val.length() - 1)))
+                            len--;
+                        profile.setAge(Integer.parseInt(val.substring(0, len)));
                     }else if(tp.toString().contains("Email:")) {
-                        profile.setEmail(val.substring(0, val.length() - 1));
+                        if(!Character.isDigit(val.charAt(len - 1)) && !Character.isAlphabetic(val.charAt(len - 1)))
+                            len--;
+                        profile.setEmail(val.substring(0, len));
                     }else if(tp.toString().contains("Phone:")) {
-                        if(Character.isDigit(val.charAt(val.length() - 1)))
-                            profile.setPhone(Long.parseLong(val.toString()));
-                        else profile.setPhone(Long.parseLong(val.substring(0, val.length() - 1)));
+                        if(!Character.isDigit(val.charAt(val.length() - 1)))
+                            len--;
+                        profile.setPhone(Long.parseLong(val.substring(0, len)));
                     }
                     tp = new StringBuilder("");
                     val = new StringBuilder("");
@@ -37,7 +41,7 @@ public class FileReader {
                 }else if((char)c == ' '){
                     has = true;
                 }
-                if(has && (char)c != ' '){
+                if(has && (char)c != ' ' && c != 10){
                     val.append((char)c);
                 }else if(!has){
                     tp.append((char)c);
