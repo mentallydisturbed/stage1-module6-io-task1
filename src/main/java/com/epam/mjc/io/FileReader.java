d131af4d8ed12b18ccc8c69cba7e0dae5f08a2d5
mentallydisturbed <magzhan.zhumagazy@gmail.com>
2023-05-27 01:41:22 +0600

    mod6task1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,48 @@
 package com.epam.mjc.io;
 
+
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        FileWriter fileWriter = null;
+        Profile profile = new Profile();
+        try(java.io.FileReader in = new java.io.FileReader(file)) {
+            int c;
+            String tp = "";
+            String val = "";
+            boolean has = false;
+            while((c = in.read()) != -1){
+                if((char)(c) == '\n') {
+                    if(tp.contains("Name:")){
+                        profile.setName(val.substring(0, val.length() - 1));
+                    }else if(tp.contains("Age:")) {
+                        profile.setAge(Integer.parseInt(val.substring(0, val.length() - 1)));
+                    }else if(tp.contains("Email:")) {
+                        profile.setEmail(val.substring(0, val.length() - 1));
+                    }else if(tp.contains("Phone:")) {
+                        profile.setPhone(Long.parseLong(val.substring(0, val.length() - 1)));
+                    }
+                    tp = "";
+                    val = "";
+                    has = false;
+                    continue;
+                }else if((char)c == ' '){
+                    has = true;
+                }
+                if(has && (char)c != ' '){
+                    val += (char)c;
+                }else if(!has){
+                    tp += (char)c;
+                }
+            }
+        } catch(IOException e) {
+            System.out.println(e);
+        }
+        return profile;
     }
 }
